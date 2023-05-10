062d1e2b46ef9c82de7b37b23ed9af46f068246d
fevzieness <fevzieness1@gmail.com>
2023-05-10 14:07:27 +0300

    Update kdv.java

--- kdv.java
+++ kdv.java
@@ -1,4 +1,5 @@
-//
+// Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan program yazınız.
+
 
 import java.util.Scanner;
 
